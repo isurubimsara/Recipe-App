c24287c088bfc4707415e1282a153ee6ae41fe90
Isuru Bimsara <bimsaragunasekara@gmail.com>
2023-05-20 16:11:40 +0530

    commit01

---
+++ server/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import {userRouter } from './routes/users.js'
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth",userRouter)
+
+mongoose.connect(
+  "mongodb+srv://isuru:recipeapp@cluster0.kjdmsus.mongodb.net/?retryWrites=true&w=majority",
+).then(() => {
+    console.log("mongoDB sucessfully conected")
+}).catch((err) => {
+    console.log("error" + err)
+})
+
+app.listen(3001, () => {
+    console.log("Server started")
+});
